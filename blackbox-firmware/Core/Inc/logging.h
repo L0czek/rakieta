68fc0368e2fb8890834a4bfcaeb36ebb7701fcfc
Michał Szaknis <michalszak@gmail.com>
2026-01-09 01:17:23 +0100

    base code

---
+++ blackbox-firmware/Core/Inc/logging.h
@@ -0,0 +1,14 @@
+#ifndef __LOGGING_H__
+#define __LOGGING_H__
+
+#ifdef __cpluspluc
+extern "C" {
+#endif
+
+void LogError(const char *, ...);
+
+#ifdef __cpluspluc
+}
+#endif
+
+#endif
---
+++ blackbox-firmware/Core/Src/logging.c
@@ -0,0 +1,5 @@
+#include "logging.h"
+
+void LogError(const char *, ...) {
+
+}
